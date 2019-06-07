c898c26d14efaa3439bad818d16c6985b36aff11
Michele Locati <michele@locati.it>
2019-06-07 11:42:50 +0200

    Add comments

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,2 +1,12 @@
+<#
+  PowerShell startup script.
+
+  Save as %USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
+  You may need to "Unblock" this file if you download it from internet (file properties -> Unblock)
+#>
+
+# Make PowerShell downloading files much faster
 $global:ProgressPreference='SilentlyContinue'
+
+# `Ctrl`+`D` key combination to close the currently open PowerShell terminal (like Linux)
 Set-PSReadlineKeyHandler -Key ctrl+d -Function ViExit
